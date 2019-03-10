e79097c54687882ce9e267b26ef571d06368c95b
Will Jarvis <will@toohotlimited.com>
2019-03-10 16:25:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = '';
+const shortGreeting = 'Welcome, ';
