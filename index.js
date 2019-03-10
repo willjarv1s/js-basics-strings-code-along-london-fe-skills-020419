d11c1e4a7528d235c7f4d8405f4f626f98a679cb
Will Jarvis <will@toohotlimited.com>
2019-03-10 16:24:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-
+const currentUser = 'Edsger Dijkstra';
+ 
+...
+ 
+const shortGreeting = ... ;
+ 
+shortGreeting;
